5ec1b9e907be855e67ae98de018e720458e57a27
Beauwie <kvn.philippe@gmail.com>
2018-04-13 13:43:38 +0200

    Add gzip compression

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const path = require("path");
 const express = require("express");
+const compression = require("compression");
 const app = express();
 const publicPath = path.join(__dirname, "..", "build");
 const port = process.env.PORT || 3001;
 
+app.use(compression());
 app.use(express.static(publicPath));
 
+console.log("helloworld");
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
+  console.log("servingindex.html");
 });
 
 app.listen(port, () => {
